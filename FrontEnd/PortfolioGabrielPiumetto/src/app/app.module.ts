55ab15c631c40d006763977be12305df2a29a6ab
Gabriel Piumetto <gabo125@gmail.com>
2022-10-25 12:16:42 -0300

    crean componentes de las secciones del portfolio

--- FrontEnd/PortfolioGabrielPiumetto/src/app/app.module.ts
+++ FrontEnd/PortfolioGabrielPiumetto/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaLaboralComponent } from './componentes/experiencia-laboral/experiencia-laboral.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HardAndSoftSkillsComponent } from './componentes/hard-and-soft-skills/hard-and-soft-skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciaLaboralComponent,
+    EducacionComponent,
+    HardAndSoftSkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
