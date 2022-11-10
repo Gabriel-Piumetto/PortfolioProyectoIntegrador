1c6e2ed0fbfa663f803dba81acfd4a75f7a0592e
Gabriel Piumetto <gabo125@gmail.com>
2022-11-10 10:24:54 +0000

    se deja funcionando la conexion con la db mysql, se agregan los packages

--- BackEnd/src/main/java/com/portfolio/gabriel/piumetto/GabrielPiumettoApplication.java
+++ BackEnd/src/main/java/com/portfolio/gabriel/piumetto/GabrielPiumettoApplication.java
@@ -1,13 +1,13 @@
-package com.portfoliogabrielpiumetto;
+package com.portfolio.gabriel.piumetto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class GabrielPiumettoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(GabrielPiumettoApplication.class, args);
 	}
 
 }
--- BackEnd/src/test/java/com/portfolio/gabriel/piumetto/GabrielPiumettoApplicationTests.java
+++ BackEnd/src/test/java/com/portfolio/gabriel/piumetto/GabrielPiumettoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.portfoliogabrielpiumetto;
+package com.portfolio.gabriel.piumetto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class GabrielPiumettoApplicationTests {
 
 	@Test
 	void contextLoads() {
