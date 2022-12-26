f5c2c70cf2280b53674f52ed90cfdcb4f73e9df0
Gabriel Piumetto <gabo125@gmail.com>
2022-12-26 22:16:33 +0000

    se hace el back end para poder traer/crear/actualizar/ items en Exp.Laboral, prueba en Postman dan bien
    se hace el back end para poder traer/crear/actualizar/ items en Exp.Laboral, prueba en Postman dan bien

---
+++ BackEnd/src/main/java/com/portfolio/gabriel/piumetto/Dto/dtoExperiencia.java
@@ -0,0 +1,40 @@
+package com.portfolio.gabriel.piumetto.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class dtoExperiencia {
+    @NotBlank
+    private String nombreE;
+    @NotBlank
+    private String descripcionE;
+    
+//Constructores
+
+    public dtoExperiencia() {
+    }
+
+    public dtoExperiencia(String nombreE, String descripcionE) {
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+    }
+
+//Getters and Setters
+
+    public String getNombreE() {
+        return nombreE;
+    }
+
+    public void setNombreE(String nombreE) {
+        this.nombreE = nombreE;
+    }
+
+    public String getDescripcionE() {
+        return descripcionE;
+    }
+
+    public void setDescripcionE(String descripcionE) {
+        this.descripcionE = descripcionE;
+    }
+
+    
+}
