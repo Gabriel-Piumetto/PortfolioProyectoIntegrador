562f756de82eef3a1c1708f5ba2076aef6156cd5
Gabriel Piumetto <gabo125@gmail.com>
2023-01-12 15:12:08 +0000

    se deja el component Acercade funcionando

--- FrontEnd/PortfolioGabrielPiumetto/src/app/componentes/acerca-de/edit-acerca-de.component.ts
+++ FrontEnd/PortfolioGabrielPiumetto/src/app/componentes/acerca-de/edit-acerca-de.component.ts
@@ -27,6 +27,7 @@ export class EditAcercaDeComponent implements OnInit {
 
   onUpdate(): void{
     const id = this.activatedRoute.snapshot.params['id'];
+    this.persona.img = this.imageService.url
     this.personaService.update(id, this.persona).subscribe(
       data => {this.router.navigate(['']);
     }, err =>{
